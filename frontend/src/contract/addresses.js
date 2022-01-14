b648fb8f17647a3b1225e67937008656bf138abd
ali-ahmed-05 <aliahmed.pluton@gmail.com>
2022-01-14 21:38:05 +0500

    Updated

--- frontend/src/contract/addresses.js
+++ frontend/src/contract/addresses.js
@@ -1,5 +1,5 @@
 
- export const nft_addr = "0x7e6ac74B047dE11ec4444C32F23728FB36B690B6"
- export const nftPreSale_addr = "0xD4d3776CD516E71Cf2A13999d5Fd43fDAfeEef78"
- export const nFTpaymentSplitter_addr = "0x8106192A3e387bBA894629544799f59b11a07cC3"
+ export const nft_addr = "0x6753270dD16aC76e51Dbcb635CE55505313AeB77"
+ export const nftPreSale_addr = "0xDa056FFE9FfE5DCC819a461FF0dC6bAb79C1C534"
+ export const nFTpaymentSplitter_addr = "0x2640962E2D161b1d5de46f5Ee21f0C2a221e5f09"
  export const BUSD_addr = "0xca9Eb9DDC1B511d5B67d833D1d36Ee85503DA93E"
